2a6d4e78de7d56595b26a7e8f7291cd7734fe038
pengshixin.psx <pengshixin.psx@alibaba-inc.com>
2025-12-05 07:28:09 +0000

    refactor: improve test setup and teardown in env tests

--- tests/unit/test_envs.py
+++ tests/unit/test_envs.py
@@ -2,9 +2,16 @@ from rock import env_vars
 
 
 def test_default_envs():
-    log_dir = "/data/log"
-    env_vars.ROCK_LOGGING_PATH = log_dir
-    assert log_dir == env_vars.ROCK_LOGGING_PATH
+    # Setup
+    original_value = env_vars.ROCK_LOGGING_PATH
+
+    try:
+        log_dir = "/data/log"
+        env_vars.ROCK_LOGGING_PATH = log_dir
+        assert log_dir == env_vars.ROCK_LOGGING_PATH
+    finally:
+        # Teardown
+        env_vars.ROCK_LOGGING_PATH = original_value
 
 
 def test_envs_project_root():
