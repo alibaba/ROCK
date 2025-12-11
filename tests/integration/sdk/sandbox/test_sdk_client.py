469f1e9b708322a1756e3893f889cca720928bef
Zone <38124819+zhongwen666@users.noreply.github.com>
2025-12-11 14:32:59 +0800

    throw exp if not start (#121)

--- tests/integration/sdk/sandbox/test_sdk_client.py
+++ tests/integration/sdk/sandbox/test_sdk_client.py
@@ -3,6 +3,7 @@ import time
 import pytest
 
 from rock.sdk.sandbox.client import Sandbox
+from rock.sdk.sandbox.config import SandboxConfig
 from tests.integration.conftest import SKIP_IF_NO_DOCKER
 
 
@@ -35,6 +36,23 @@ async def test_arun_timeout(sandbox_instance: Sandbox):
 
     await sandbox_instance.stop()
 
+@pytest.mark.need_admin
+@SKIP_IF_NO_DOCKER
+@pytest.mark.asyncio
+async def test_sandbox_get_status(admin_remote_server):
+    config = SandboxConfig(
+        image="fake_image:latest",
+        memory="8g",
+        cpus=2.0,
+        base_url=f"{admin_remote_server.endpoint}:{admin_remote_server.port}",
+        startup_timeout=10,
+    )
+    sandbox = Sandbox(config)
+    with pytest.raises(Exception) as exc_info:
+        await sandbox.start()
+    assert "Failed to start sandbox" in str(exc_info.value)
+    sandbox.stop()
+    
 @pytest.mark.need_admin
 @SKIP_IF_NO_DOCKER
 @pytest.mark.asyncio
