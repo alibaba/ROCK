87c5edbd9af9d2e4702173f1b23298b115c9b90f
Jiachen Zhang <zjc462490@alibaba-inc.com>
2026-06-17 09:53:26 +0000

    refactor(sandbox): rename state_enter_time to intermediate_state_started_at

--- rock/admin/core/schema.py
+++ rock/admin/core/schema.py
@@ -48,7 +48,7 @@ class SandboxRecord(Base):
     disk = Column(String(64), nullable=True)
     create_user_gray_flag = Column(Boolean, nullable=True)
     archive_time = Column(String(64), nullable=True)
-    state_enter_time = Column(String(64), nullable=True)
+    intermediate_state_started_at = Column(String(64), nullable=True)
     delete_time = Column(String(64), nullable=True)
     phases = Column(_JSONB_VARIANT, nullable=True)
     port_mapping = Column(_JSONB_VARIANT, nullable=True)
